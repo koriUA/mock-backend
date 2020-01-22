a2d6013e401709c5247752f73a1eabc217db4f3d
vitalii <kori.if.ua@gmail.com>
2020-01-22 14:05:03 +0200

    edit dashboard

--- index.js
+++ index.js
@@ -37,14 +37,13 @@ app.get("/fetch-report-details-by-id", (req, res, next) => {
 });
 
 app.get("/dashboards", (req, res, next) => {
-  res.send([
-    { id: 1, title: "Main Dashboard" },
-    { id: 2, title: "Dashboard 001" },
-    { id: 3, title: "My Dashboard" },
-    { id: 4, title: "KPI Trends" },
-    { id: 5, title: "Merchandise" },
-    { id: 6, title: "Session Analitics" }
-  ]);
+  const data = Object.keys(dashboardsData.data).map((key) => {
+    return {
+      id: dashboardsData.data[key].id,
+      title: dashboardsData.data[key].title,
+    }
+  });
+  res.send(data);
 });
 
 app.get("/dashboards/:id", (req, res, next) => {
